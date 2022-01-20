84a992cabdec95e4aac12329ec919577d2bf3adb
Vincent Fücks <57221743+Vincent-Fuecks@users.noreply.github.com>
2022-01-20 14:31:32 +0100

    Update evaluation-measures.py

--- utils/evaluation-measures.py
+++ utils/evaluation-measures.py
@@ -33,9 +33,14 @@ class evaluation_measures():
         c_result  : Is a tupel, which contains the separated data of each cluster
         '''    
         all_purity = ([None]*len(c_result))
+        purity = 0
+        purity_j = 0
         for i, C in enumerate(c_result):
-            tmp = np.sum(C, axis = 0)
-            all_purity[i] =  (1/np.sum(tmp)) * np.max(tmp)
+            m = np.sum(C, axis = 0)
+            for m_j in m:
+                purity_j = m_j/np.sum(m) if m_j/np.sum(m) > purity_j else purity_j
+             
+            all_purity[i] = m_j/np.sum(m) * purity_j 
             print("Purity for Cluster %d is %f" %(i, all_purity[i]))
         print("Purity is %f \n" %( np.sum(all_purity)))
 
@@ -48,13 +53,15 @@ class evaluation_measures():
         ------------
         c_result  : Is a tupel, which contains the separated data of each cluster
         '''    
-        all_e = np.array([None]*len(c_result))
+        entropy = 0
         for i, C in enumerate(c_result):
             m = np.sum(C, axis = 0)
             e_j = 0
             for m_j in m: 
                 log = 0 if m_j == 0 or np.sum(m) == 0 else np.log(m_j / np.sum(m))   
                 e_j += m_j/np.sum(m) * log
-            all_e[i] = e_j*-1
-            print("Entropy for Cluster %d is %f" %(i, all_e[i]))
-        print("Purity is %f \n" %(np.sum(all_e)))
+            all_j[i] = e_j*-1
+            entropy += m_j/np.sum(m) * all_j[i]
+        
+            print("Entropy for Cluster %d is %f" %(i, all_j[i]))
+        print("Entropy is %f \n" %(entropy))
