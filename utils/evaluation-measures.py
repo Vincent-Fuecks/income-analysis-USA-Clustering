1d5a3c49560877b2973860f79f827c63dcf6c4b8
Vincent Fücks <57221743+Vincent-Fuecks@users.noreply.github.com>
2022-01-20 16:42:21 +0100

    Update evaluation-measures.py

--- utils/evaluation-measures.py
+++ utils/evaluation-measures.py
@@ -6,7 +6,7 @@ from sklearn_extra.cluster import KMedoids
 
 
 class evaluation_measures():
-    def clustering_results(self, df, model):
+    def clustering_results(self, df, best_model, model:
         '''
         This function clusters the data with given model and returns 
         the data of each cluster in an array.
@@ -15,14 +15,20 @@ class evaluation_measures():
         ------------
         df        : DataFrame
                     Contains data for clustering.
-        model     :Is a sklearn clustering model.  
+        best_model: np.array([]) Has index of best fold
+        model     : Is a sklearn clustering model.  
         Returns
         ------------
         c_result  : Return in a tupel the data of each cluster in an array.
         '''        
-        X_train = np.array(df)[:-1]
-        y_pred = model.fit(X_train).labels_
-        return (X_train[y_pred], X_train[0 == y_pred])
+        data = np.array(df.iloc[best_model[0]])
+        features = data[:, :-1]
+        target = data[:, -1]
+        y_pred = model.fit(features).predict(features)
+        total = target.shape[0]
+        cluster_0 = np.sum([y_t == y and y ==1  for y_t, y in zip(target, y_pred)])
+        cluster_1 = [y_t == y and y == 0 for y_t, y in zip(target, y_pred)]
+        return cluster_0, cluster_1, total
         
 
     def cluster_purity(self, c_result):
@@ -30,19 +36,11 @@ class evaluation_measures():
         This function calculates for each cluster the purity.
         Parameters
         ------------
-        c_result  : Is a tupel, which contains the separated data of each cluster
+        c_result  : Is a tupel, which contains cluster_0, cluster_0, total 
         '''    
-        all_purity = ([None]*len(c_result))
-        purity = 0
-        purity_j = 0
-        for i, C in enumerate(c_result):
-            m = np.sum(C, axis = 0)
-            for m_j in m:
-                purity_j = m_j/np.sum(m) if m_j/np.sum(m) > purity_j else purity_j
-             
-            all_purity[i] = m_j/np.sum(m) * purity_j 
-            print("Purity for Cluster %d is %f" %(i, all_purity[i]))
-        print("Purity is %f \n" %( np.sum(all_purity)))
+        cluster_0, cluster_1, total = c_result
+        print(cluster_0)
+        return (np.sum(cluster_0) + np.sum(cluster_1))/ total
 
 
 
@@ -51,18 +49,7 @@ class evaluation_measures():
         This function calculates for each cluster the entropy.
         Parameters
         ------------
-        c_result  : Is a tupel, which contains the separated data of each cluster
+        c_result  : Is a tupel, which contains cluster_0, cluster_0, total 
         '''    
-        all_j = ([None]*len(c_result))
-        entropy = 0
-        for i, C in enumerate(c_result):
-            m = np.sum(C, axis = 0)
-            e_j = 0
-            for m_j in m: 
-                log = 0 if m_j == 0 or np.sum(m) == 0 else np.log(m_j / np.sum(m))   
-                e_j += m_j/np.sum(m) * log
-            all_j[i] = e_j*-1
-            entropy += m_j/np.sum(m) * all_j[i]
-        
-            print("Entropy for Cluster %d is %f" %(i, all_j[i]))
-        print("Entropy is %f \n" %(entropy))
+        cluster_0, cluster_1, total = c_result
+        return -((np.sum(cluster_0)/total) * np.log((np.sum(cluster_0)/total) )) - ((np.sum(cluster_1)/total) * np.log((np.sum(cluster_1)/total) ))
