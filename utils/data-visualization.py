64467b1faa0871215c44888d360b39974a612396
Vincent Fücks <57221743+Vincent-Fuecks@users.noreply.github.com>
2022-01-20 10:11:46 +0100

    Update data-visualization.py
    - Added function plot_3D
    - modified function prepare_data and visualizatiion
    - changed name "modle" to "model"

--- utils/data-visualization.py
+++ utils/data-visualization.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
-
+import plotly.express as px
 
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
@@ -11,7 +11,7 @@ from sklearn.cluster import KMeans
 from sklearn_extra.cluster import KMedoids
 
 class data_visualization():
-    def prepare_data(self, df, best_modle):
+    def prepare_data(self, df, best_model, n_comp = 2):
         '''
         Visualization of decision boundaries from the classifiers.
         A dimension reduction to two features is applied, to visualize the 
@@ -23,21 +23,17 @@ class data_visualization():
                      An indexlist of the fold which has performed best overall (only need this)
         Returns
         ------------
-        features_0  :(np.array(float)) X_train of the best performing modle
-        y_0         :(np.array(float)) y_train of the best performing modle
-        features_1  :(np.array(float)) X_train of the second best performing modle
-        y_1         :(np.array(float)) y_train of the second best performing modle
+        features  :(np.array(float)) X_train of the best performing model
+        y         :(np.array(float)) y_train of the best performing model
         '''
-        arr_0 = np.array(df.iloc[best_modle[0]])
-        arr_1 = np.array(df.iloc[best_modle[1]])
-        features_0 = self.dimension_reduction_to_2D(arr_0[:, :-1])
-        features_1 = self.dimension_reduction_to_2D(arr_1[:, :-1])
-        y_0 = arr_0[:, -1]
-        y_1 = arr_1[:, -1]
-        return features_0, y_0, features_1, y_1
+        arr = np.array(df.iloc[best_model[0]])
+        features = self.dimension_reduction_to_n_dimension(arr[:, :-1], n_comp)
+        return features, y = arr[:, -1]
 
 
-    def visualization(self, df, best_model, modle):
+
+
+    def visualization(self, df, best_model, model):
         '''
         Visualization of decision boundaries from the classifiers.
         A dimension reduction to two features is applied, to visualize the 
@@ -47,14 +43,49 @@ class data_visualization():
                     Your datatable
         best_model  :(np.array(int))
                     An indexlist of the fold which has performed best overall (only need this)
-        modle:      is classifier with the parameters, of the best fold 
+        model:      is classifier with the parameters, of the best fold 
         '''
-        f0_redu, y_0, f1_redu, y_1 =  self.prepare_data(df, best_model)
-        self.plot_decision_boundary2D(f0_redu, y_0, modle)
-        self.plot_decision_boundary2D(f1_redu, y_1, modle)
+        f_redu, y =  self.prepare_data(df, best_model)
+        self.plot_decision_boundary2D(f_redu, y, model)
+        print("\n")
+        f_redu, y =  self.prepare_data(df, best_model, 3)
+        self.plot_3D(f_redu, y, model)
 
+    def plot_3D(self, x_data, y_data, model):
+        """
+        Fist plot reducd best fold data, Secound plot best fold clustered with best parameters in 3D
+        ------------
+        x_data      :(np.array(float))  X_train for the best fold 
+        y_data      :(np.array(float)) target data of the feature data  
+        model       : scikit learn model 
+        """
+        df = pd.DataFrame(data = np.c_[x_data, y_data], columns=['x', 'y', 'z', 'color'])
+        fig = px.scatter_3d(df, x='x', y='y', z='z', color='color', 
+                             labels={
+                                    "x": "feature 0",
+                                    "y": "feature 1",
+                                    "z": "feature 2" },
+                            title="Adult dataset best fold reduced to 3D")
+        fig.show()
+        
 
-    def dimension_reduction_to_2D(self, data):
+        arr_0 = np.array(df)
+        features_0 = self.dimension_reduction_to_n_dimension(arr_0[:, :-1], 3)
+        y_0 = arr_0[:, -1]
+        model.fit(features_0)
+        y_pred = model.predict(x_data)
+        df = pd.DataFrame(data = np.c_[x_data, y_pred], columns=['x', 'y', 'z', 'color'])
+        print("\n")
+        fig = px.scatter_3d(df, x='x', y='y', z='z', color='color',
+                            labels={
+                                    "x": "feature 0",
+                                    "y": "feature 1",
+                                    "z": "feature 2" },
+                            title= "Adult dataset best fold reduced to 3D, clustered with " + str(model) )
+        fig.show()
+        
+        
+    def dimension_reduction_to_n_dimension(self, data, n_comp = 2):
         """
         Performs a dimension reduction to 2 components. Use StandardScaler, for 
         a nicer visualization. For the reduction is used PCA. 
@@ -65,12 +96,12 @@ class data_visualization():
         principalComponents : (np.array(float)) data of only to features 
         """
         x = StandardScaler().fit_transform(data)
-        pca = PCA(n_components=2)
+        pca = PCA(n_components= n_comp)
         principalComponents = pca.fit_transform(x)
         return principalComponents
     
 
-    def plot_decision_boundary2D(self,features, target, modle):
+    def plot_decision_boundary2D(self,features, target, model):
         """
         Plots the feature points and draws the decision boundaries from the classifier.
         We used part of the code of the Website for this function:
@@ -78,10 +109,10 @@ class data_visualization():
         ------------
         features    :(np.array(float))  X_train for the best fold 
         target      :(np.array(float)) target data of the feature data  
-        modle       : scikit learn modle 
+        model       : scikit learn model 
         """
         y = np.array(target).flatten()
-        modle.fit(features, target)
+        model.fit(features, target)
 
        
 
@@ -101,10 +132,10 @@ class data_visualization():
                             np.arange(y_min, y_max, 0.1))
 
         # Predictions to obtain the classification results
-        Z = modle.predict(np.c_[xx.ravel(), yy.ravel()]).reshape(xx.shape)
+        Z = model.predict(np.c_[xx.ravel(), yy.ravel()]).reshape(xx.shape)
 
         # Plotting
-        plt.title(modle)
+        plt.title(model)
         colors = ['orange', 'indigo']
         co = [colors[x] for x in y.astype(int)]
         plt.contourf(xx, yy, Z, alpha=0.5)
